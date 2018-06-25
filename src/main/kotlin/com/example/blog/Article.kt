a81be36ecdc1dbd5d4dd674debed0ce49c8465ad
fabricio-lemos <fabricio.lemos@rbc.com>
2018-06-25 16:08:41 -0400

    add optional Article properties

--- src/main/kotlin/com/example/blog/Article.kt
+++ src/main/kotlin/com/example/blog/Article.kt
@@ -1,4 +1,8 @@
 package com.example.blog
 
+import java.time.LocalDateTime
+
 data class Article(val title: String,
-                   val content: String)
+                   val content: String,
+                   val createdAt: LocalDateTime = LocalDateTime.now(),
+                   val id: Long? = null)
